8e30333ff8aeb187a10bffae215788ca6cd6ab10
hemalhansda <hemal.hansda25@gmail.com>
2018-06-23 14:22:27 +0530

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Heroku',
+    text: 'Hey bro whats going on...',
+    createdAt: 1213
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
