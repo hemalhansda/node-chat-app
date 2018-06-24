6bf1ec0a8cc44b64b4cb2ca4d2cf696bd54b28ae
hemalhansda <hemal.hansda25@gmail.com>
2018-06-24 17:06:52 +0530

    some update

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
     users.addUser(socket.id, params.name, params.room);
 
     io.to(params.room).emit('updateUserList', users.getUserList(params.room));
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to DoChat :-)'));
     socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
 
     callback();
